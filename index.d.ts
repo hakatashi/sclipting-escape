d63a4ff67eede5323a41c671f819c0819b22d7a7
Koki Takahashi <hakatasiloving@gmail.com>
2015-11-24 15:16:17 +0900

    Include .d.ts files from `tsc --declaration`

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="typings/tsd.d.ts" />
+export declare const encode: (data: Buffer) => string;
+export declare const decode: (text: string) => Buffer;
---
+++ util.d.ts
@@ -0,0 +1,3 @@
+export declare function ord(char: string): number;
+export declare function chr(codePoint: number): string;
+export declare function zfill(string: string, length: number): string;
